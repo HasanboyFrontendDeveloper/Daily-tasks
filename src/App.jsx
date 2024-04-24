6f8c0bc741518eff2fb0f0658f65274a4b08f122
Hasanboy <hasanboyturdijonov@gmail.com>
2024-04-24 14:28:02 +0500

    started

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
-import { Login } from "./components";
 
 const App = () => {
   return (
-    <div>
-      <Login />
+    <div className="flex justify-center items-center h-[100vh] bg-red-500">
+      <div className="text-white text-2xl text-center">
+        Welcome To 🎯 Daily Tasks
+        <h3>Good Luck with Project</h3>
+      </div>
     </div>
   );
 };
