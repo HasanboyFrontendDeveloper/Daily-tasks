7d7b9a0d544354d89b389798ab72e4ed7a06e892
dovudbek <111281179+dovudbek7@users.noreply.github.com>
2024-04-24 21:08:22 +0500

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,30 @@
 import React from "react";
+import './App.css';
 
 const App = () => {
   return (
-    <div className="flex justify-center items-center h-[100vh] bg-red-500">
-      <div className="text-white text-2xl text-center">
-        Welcome To 🎯 Daily Tasks
-        <h3>Good Luck with Project</h3>
-        <p>Hello </p>
+    <div>
+      <nav className="h-10 w-full"> 
+        <p className="float-left w-28 font-bold text-xl ">Daily Tasks</p> 
+        <button className="float-right  bg-black text-white w-28  h-8 rounded ">Sign in</button> 
+      </nav>
+      <hr />
+
+      <div className="flex items-center justify-center">
+      <div className="border-2  w-[75%] lg:w-[35%] md:w-[50%] mt-[100px] pl-4 pt-3  rounded  ">
+        <p className="text-[20px] f-bold font-bold ">Sign in</p>
+        <p>Nice to meet you! Enter your email to login</p>
+        <form action="" className="pl-[2%]" > <br />
+          <label className="" htmlFor="name">Your email</label> <br />
+          <input className="rounded border-2 pl-3 h-10 w-[90%] mt-2" type="text" name="" id="name" /> <br />
+        </form>
+        <div className="text-center">
+        <button className="bg-black text-white w-28  h-8 rounded  mt-10    ">Sign in</button> 
       </div>
+      <br />
+      </div>
+      </div>
+
     </div>
   );
 };
